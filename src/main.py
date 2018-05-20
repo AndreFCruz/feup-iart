0aa5050ea842a23806a415fb2b6f3b598ce7faa6
AndreFCruz <andrecruz97@gmail.com>
2018-05-20 23:30:27 +0100

    minor changes, mostly graphs and logs

--- src/main.py
+++ src/main.py
@@ -21,15 +21,13 @@ X_train, Y_train = train_set[:, :-1], train_set[:, -1]
 X_test, Y_test = test_set[:, :-1], test_set[:, -1]
 
 
-## NOTE Undersampling!
-from imblearn.under_sampling import RandomUnderSampler
-rus = RandomUnderSampler(return_indices=True)
-X_resampled, Y_resampled, idx_resampled = rus.fit_sample(pulsars[:,:-1], pulsars[:,-1])
-print("Original data size: %d. Undersampled data size: %d" % (len(pulsars), len(X_resampled)))
+## Optionally: Undersample Majority Class
+# from imblearn.under_sampling import RandomUnderSampler
+# rus = RandomUnderSampler(return_indices=True)
+# X_resampled, Y_resampled, idx_resampled = rus.fit_sample(pulsars[:,:-1], pulsars[:,-1])
+# print("Original data size: %d. Undersampled data size: %d" % (len(pulsars), len(X_resampled)))
 
-X_train, Y_train = X_resampled, Y_resampled
-
-## NOTE END of Undersampling
+# X_train, Y_train = X_resampled, Y_resampled
 
 
 # Create Model
@@ -37,17 +35,17 @@ model = create_model(np.size(X_train, axis=1))
 
 # Train Model
 callbacks = [
-        PRTensorBoard(log_dir=('../Graph/' + MODEL_NAME), write_images=True, histogram_freq=5)
-        # EarlyStopping(monitor='val_acc', patience=20)
+        PRTensorBoard(log_dir=('../Graph/' + MODEL_NAME), write_images=True, histogram_freq=5),
+        EarlyStopping(monitor='val_acc', patience=20)
         ]
 
-## NOTE Balancing class weights
-from sklearn.utils import class_weight
-class_weight = class_weight.compute_class_weight('balanced',
-                                                 np.unique(Y_train),
-                                                 Y_train)
-print("Class weight: ", class_weight)
-#
+## Optionally: Balance class weights
+# from sklearn.utils import class_weight
+# class_weight = class_weight.compute_class_weight('balanced',
+#                                                  np.unique(Y_train),
+#                                                  Y_train)
+# print("Class weight: ", class_weight)
+
 
 # Train Model
 model.fit(X_train, Y_train, epochs=100, batch_size=16,
@@ -62,11 +60,10 @@ logger = Logger(LOGS_FILE)
 scores = model.evaluate(X_test, Y_test)
 print("Overall Accuracy: %.2f\n" % (scores[1] * 100))
 print(evaluate_classwise(model, X_test, Y_test))
-# print("Classwise Results: ", evaluate_model(model, X_test, Y_test))
 logger.close()
 
 
-## NOTE K-Fold Cross-Validation
+## K-Fold Cross-Validation
 # from utils import train_model, evaluate_model
 # ret = cross_validate(model, pulsars[:,:-1], pulsars[:,-1], 10,
 #         train_model,
