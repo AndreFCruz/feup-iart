f9ca6214409cb257d580b5f305cf2ee2dab216ac
AndreFCruz <andrecruz97@gmail.com>
2018-05-20 15:33:54 +0100

    images and logs for report

--- src/main.py
+++ src/main.py
@@ -20,27 +20,45 @@ train_set, test_set = train_test_split(pulsars, 0.2)
 X_train, Y_train = train_set[:, :-1], train_set[:, -1]
 X_test, Y_test = test_set[:, :-1], test_set[:, -1]
 
+
+## NOTE Undersampling!
+from imblearn.under_sampling import RandomUnderSampler
+rus = RandomUnderSampler(return_indices=True)
+X_resampled, Y_resampled, idx_resampled = rus.fit_sample(pulsars[:,:-1], pulsars[:,-1])
+print("Original data size: %d. Undersampled data size: %d" % (len(pulsars), len(X_resampled)))
+
+shuffled_indices = np.random.permutation(len(X_resampled))
+test_set_size = int(0.2 * len(X_resampled))
+test_indices = shuffled_indices[:test_set_size]
+train_indices = shuffled_indices[test_set_size:]
+X_train, Y_train = X_resampled[train_indices], Y_resampled[train_indices]
+X_test, Y_test = X_resampled[test_indices], Y_resampled[test_indices]
+
+
+## NOTE END of Undersampling
+
 # Create Model
 model = create_model(np.size(X_train, axis=1))
 
 # Train Model
 callbacks = [
-        PRTensorBoard(log_dir=('../Graph/' + MODEL_NAME), write_images=True),
-        EarlyStopping(monitor='val_acc', patience=15)
+        PRTensorBoard(log_dir=('../Graph/' + MODEL_NAME), write_images=True, histogram_freq=5)
+        # EarlyStopping(monitor='val_acc', patience=20)
         ]
 
-### NOTE Balancing class weights
+## NOTE Balancing class weights
 # from sklearn.utils import class_weight
 # class_weight = class_weight.compute_class_weight('balanced',
 #                                                  np.unique(Y_train),
 #                                                  Y_train)
 # print("Class weight: ", class_weight)
-###
+##
 
 # Train Model
 model.fit(X_train, Y_train, epochs=100, batch_size=16,
         validation_data=(X_test, Y_test),
         callbacks=callbacks
+        # class_weight=class_weight
         )
 
 
@@ -53,19 +71,12 @@ print(evaluate_classwise(model, X_test, Y_test))
 logger.close()
 
 
-## NOTE Undersampling!
-# from imblearn.under_sampling import RandomUnderSampler
-# rus = RandomUnderSampler(return_indices=True)
-# X_resampled, y_resampled, idx_resampled = rus.fit_sample(pulsars[:,:-1], pulsars[:,-1])
-
 ## NOTE K-Fold Cross-Validation
 # from utils import train_model, evaluate_model
 # ret = cross_validate(model, pulsars[:,:-1], pulsars[:,-1], 10,
 #         train_model,
 #         evaluate_model)
-
 # print("Result: ", ret)
-## NOTE END
 
 # print('Saving model...')
 # save_model(model, MODEL_NAME, scores[1] * 100)
